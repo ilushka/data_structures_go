ac0933a8bbc46780b81736ad1aee642fce3ce279
ilya moskovko <ilya@moskovko.com>
2019-06-05 19:58:38 -0700

    ** added quicksort **

--- linked_list.go
+++ linked_list.go
@@ -1,5 +1,9 @@
 package data_structures_go
 
+import (
+    "fmt"
+)
+  
 type LLNode struct {
     value   int
     next    *LLNode
@@ -7,28 +11,28 @@ type LLNode struct {
 
 type LinkedList struct {
     head  *LLNode
-    size  int
+    Length  int
 }
 
 func (ll *LinkedList) AppendTail(value int) {
     if ll.head == nil {
         ll.head = new(LLNode)
         ll.head.value = value
-        ll.size = 1
+        ll.Length = 1
     } else {
         nn := ll.head
         for ; nn.next != nil; nn = nn.next {}
         nn.next = new(LLNode)
         nn.next.value = value
-        ll.size++
+        ll.Length++
     }
 }
 
-func (ll *LinkedList) ToArray() *[]int {
-    if ll.size == 0 {
+func (ll *LinkedList) ToArray() (*[]int) {
+    if ll.Length == 0 {
         return nil
     }
-    a := make([]int, ll.size)
+    a := make([]int, ll.Length)
     var ii int = 0
     for nn := ll.head; nn != nil; nn = nn.next {
         a[ii] = nn.value
@@ -36,3 +40,98 @@ func (ll *LinkedList) ToArray() *[]int {
     }
     return &a
 }
+
+func get_node_at_index(head *LLNode, index int) (*LLNode) {
+    if index == 0 || head == nil {
+        return head
+    }
+    return get_node_at_index(head.next, index - 1)
+}
+
+func print_list(head *LLNode) {
+    for ; head != nil; head = head.next {
+        fmt.Printf("v: %d, ", head.value)
+    }
+    fmt.Printf("\n")
+}
+
+func qs_partition(head *LLNode, tail *LLNode) (*LLNode, *LLNode, *LLNode, *LLNode, *LLNode) {
+    if head.next == nil || head == nil || tail == nil {
+        return nil, nil, nil, nil, nil
+    }
+
+    var min_hd, min_tl, max_hd, max_tl *LLNode = nil, nil, nil, nil
+    for j := head; j != tail; j = j.next {
+        if (j.value < tail.value) {
+            // add j to list with elements < pivot
+            if min_hd == nil {
+                min_hd = j
+            } else {
+                min_tl.next = j
+            }
+            min_tl = j
+        } else {
+            // add j to list with elements >= pivot
+            if max_hd == nil {
+                max_hd = j
+            } else {
+                max_tl.next = j
+            }
+            max_tl = j
+        }
+    }
+
+    // terminate min and max lists
+    tail.next = nil
+    if min_tl != nil {
+        min_tl.next = nil
+    }
+    if max_tl != nil {
+        max_tl.next = nil
+    }
+    return min_hd, min_tl, tail, max_hd, max_tl
+}
+
+func qs_sort(head *LLNode, tail *LLNode) (*LLNode, *LLNode) {
+    if head == nil {
+        return nil, nil
+    } else if head.next == nil {
+        return head, head
+    }
+
+    min_hd, min_tl, pivot, max_hd, max_tl := qs_partition(head, tail)
+    if pivot == nil {
+        return nil, nil
+    }
+
+    par0_hd, par0_tl := qs_sort(min_hd, min_tl)
+    par1_hd, par1_tl := qs_sort(max_hd, max_tl)
+
+    var new_head, new_tail *LLNode = par0_hd, par1_tl
+    if par0_hd == nil {
+        // start sorted partition with pivot
+        new_head = pivot
+    } else {
+        // point pivot to tail of sorted min partition
+        par0_tl.next = pivot
+    }
+    if par1_tl == nil {
+        // set tail of partition to pivot
+        new_tail = pivot
+        pivot.next = nil
+    } else {
+        // point pivot to head of sorted max partition
+        pivot.next = par1_hd
+    }
+    return new_head, new_tail
+}
+
+func (ll *LinkedList) QuickSort() {
+    if ll.Length == 0 || ll.Length == 1 {
+        return
+    }
+    tail := get_node_at_index(ll.head, ll.Length - 1)
+    new_head, _ := qs_sort(ll.head, tail)
+    ll.head = new_head
+}
+
--- linked_list_test.go
+++ linked_list_test.go
@@ -10,7 +10,7 @@ func TestAppendTail(t *testing.T) {
     for _, v := range testval {
         ll.AppendTail(v)
     }
-    if ll.size != len(testval) {
+    if ll.Length != len(testval) {
         t.Errorf("linked list should have %d nodes", len(testval))
     }
     for ii, vv := range *ll.ToArray() {
@@ -20,3 +20,62 @@ func TestAppendTail(t *testing.T) {
     }
 }
 
+func TestQuickSort(t *testing.T) {
+    initial := [...]int{1000, 200, -300, 400, 500, 950, 800, 880, 900}
+    expected := [...]int{-300, 200, 400, 500, 800, 880, 900, 950, 1000}
+    ll := new(LinkedList)
+    for _, v := range initial {
+        ll.AppendTail(v)
+    }
+    ll.QuickSort()
+    for ii, vv := range *ll.ToArray() {
+        if vv != expected[ii] {
+            t.Errorf("sorted linked list's node #%d should have value of %d", ii, expected[ii])
+        }
+    }
+}
+
+func TestQuickSortSingleItem(t *testing.T) {
+    initial := [...]int{1000}
+    expected := [...]int{1000}
+    ll := new(LinkedList)
+    for _, v := range initial {
+        ll.AppendTail(v)
+    }
+    ll.QuickSort()
+    for ii, vv := range *ll.ToArray() {
+        if vv != expected[ii] {
+            t.Errorf("sorted linked list's node #%d should have value of %d", ii, expected[ii])
+        }
+    }
+}
+
+func TestQuickSortEqualValues(t *testing.T) {
+    initial := [...]int{1200, 1220, 1200, 1000, 1500, 1500, 10, 10}
+    expected := [...]int{10, 10, 1000, 1200, 1200, 1220, 1500, 1500}
+    ll := new(LinkedList)
+    for _, v := range initial {
+        ll.AppendTail(v)
+    }
+    ll.QuickSort()
+    for ii, vv := range *ll.ToArray() {
+        if vv != expected[ii] {
+            t.Errorf("sorted linked list's node #%d should have value of %d", ii, expected[ii])
+        }
+    }
+}
+
+func TestQuickSortEqualValues2(t *testing.T) {
+    initial := [...]int{1200, 1200, 1200}
+    expected := [...]int{1200, 1200, 1200}
+    ll := new(LinkedList)
+    for _, v := range initial {
+        ll.AppendTail(v)
+    }
+    ll.QuickSort()
+    for ii, vv := range *ll.ToArray() {
+        if vv != expected[ii] {
+            t.Errorf("sorted linked list's node #%d should have value of %d", ii, expected[ii])
+        }
+    }
+}

6daea0d191fe8fff670fd077f7e3e2b2f94eeeb8
ilya moskovko <ilya@moskovko.com>
2019-06-09 23:46:03 -0700

    ** modified ll's interface to Append() and AppendAtIndex() **

--- linked_list/linked_list.go
+++ linked_list/linked_list.go
@@ -15,25 +15,78 @@ type LinkedList struct {
     Length  int     // Length (count of node) of linked list
 }
 
+// Create linked list from elements in a slice
 func CreateFromSlice(slice []int) *LinkedList {
     ll := new(LinkedList)
     for _, v := range slice {
-        ll.AppendTail(v)
+        ll.Append(v)
     }
     return ll
 }
 
+// Return node at specified index and, potentially, its previous node
+func get(prev *LLNode, curr *LLNode, index int) (*LLNode, *LLNode) {
+    if curr == nil || index < 0 {
+        return nil, nil
+    }
+    if index == 0 {
+        return prev, curr
+    }
+    return get(curr, curr.next, index - 1)
+}
+
+// Set first element of linked list
+func init_ll(ll *LinkedList, value int) {
+    ll.head = &LLNode{value, nil}
+    ll.tail = ll.head
+    ll.Length++
+}
+
+// Print out each value in linked list
+func print_list(head *LLNode) {
+    for ; head != nil; head = head.next {
+        fmt.Printf("v: %d, ", head.value)
+    }
+    fmt.Printf("\n")
+}
+
+// Return value at node at specified index
+func (ll *LinkedList) Get(index int) (int, error) {
+    if _, c := get(nil, ll.head, index); c == nil {
+        return 0, fmt.Errorf("Node index %d does not exist", index)
+    } else {
+        return c.value, nil
+    }
+}
+
 // Add node to tail of linked list
-func (ll *LinkedList) AppendTail(value int) {
-    if ll.head == nil {
+func (ll *LinkedList) Append(value int) {
+    if ll.Length == 0 {
         ll.head = &LLNode{value, nil}
         ll.tail = ll.head
-        ll.Length = 1
     } else {
         ll.tail.next = &LLNode{value, nil}
         ll.tail = ll.tail.next
+    }
+    ll.Length++
+}
+
+// Add node at specified index
+func (ll *LinkedList) AppendAtIndex(value int, index int) (error) {
+    if index > ll.Length || index < 0 {
+        return fmt.Errorf("Cannot insert node at index %d in list of size %d", index, ll.Length)
+    }
+    if index == ll.Length {
+        ll.Append(value)
+    } else if index == 0 {
+        ll.head = &LLNode{value, ll.head}
+        ll.Length++
+    } else {
+        p, c := get(nil, ll.head, index)
+        p.next = &LLNode{value, c}
         ll.Length++
     }
+    return nil
 }
 
 // Returns array of values in linked list
@@ -50,11 +103,4 @@ func (ll *LinkedList) ToArray() (*[]int) {
     return &a
 }
 
-// Print out each value in linked list
-func print_list(head *LLNode) {
-    for ; head != nil; head = head.next {
-        fmt.Printf("v: %d, ", head.value)
-    }
-    fmt.Printf("\n")
-}
 
--- linked_list/linked_list_test.go
+++ linked_list/linked_list_test.go
@@ -4,15 +4,66 @@ import (
     "testing"
 )
 
-func TestAppendTail(t *testing.T) {
+func TestCreateFromSlice(t *testing.T) {
     testval := [...]int{100, 200, 300, 400, 500, 600, 700, 800, 900}
     ll := CreateFromSlice(testval[:])
     if ll.Length != len(testval) {
-        t.Errorf("linked list should have %d nodes", len(testval))
+        t.Errorf("Linked list should have %d nodes", len(testval))
     }
     for ii, vv := range *ll.ToArray() {
         if vv != testval[ii] {
-            t.Errorf("linked list's node #%d should have value of %d", ii, testval[ii])
+            t.Errorf("Linked list's node #%d should have value of %d", ii, testval[ii])
+        }
+    }
+}
+
+func TestAppends(t *testing.T) {
+    ll := new(LinkedList)
+    if ll.Length != 0 {
+        t.Errorf("Linked list should be of length 0")
+    }
+
+    // Node at index 5 does not exist
+    err := ll.AppendAtIndex(555, 5)
+    if err == nil {
+        t.Errorf("Should not be able to append node at index 5 in list of length %d", ll.Length)
+    }
+
+    // Add fist node
+    ll.Append(666)
+    if ll.Length != 1 {
+        t.Errorf("Linked list should be of length 1")
+    }
+    val, err := ll.Get(0)
+    if val != 666 {
+        t.Errorf("Value at index 0 should be 666")
+    }
+
+    // Add second node specifically at index 1
+    err = ll.AppendAtIndex(777, 1)
+    if err != nil {
+        t.Errorf("Failed to append at index 1: %v", err)
+    }
+    if ll.Length != 2 {
+        t.Errorf("Linked list should be of length 2")
+    }
+    val, err = ll.Get(1)
+    if val != 777 {
+        t.Errorf("Value at index 1 should be 777")
+    }
+
+    // Append new head
+    err = ll.AppendAtIndex(555, 0)
+    if err != nil {
+        t.Errorf("Failed to append at index 0: %v", err)
+    }
+    if ll.Length != 3 {
+        t.Errorf("Linked list should be of length 3")
+    }
+    testval := [...]int{555, 666, 777}
+    for ii, vv := range *ll.ToArray() {
+        if vv != testval[ii] {
+            t.Errorf("Linked list's node #%d should have value of %d", ii, testval[ii])
         }
     }
 }
@@ -23,7 +74,7 @@ func TestQuickSort(t *testing.T) {
     ll.QuickSort()
     for ii, vv := range *ll.ToArray() {
         if vv != expected[ii] {
-            t.Errorf("sorted linked list's node #%d should have value of %d", ii, expected[ii])
+            t.Errorf("Sorted linked list's node #%d should have value of %d", ii, expected[ii])
         }
     }
 }
@@ -34,7 +85,7 @@ func TestQuickSortSingleItem(t *testing.T) {
     ll.QuickSort()
     for ii, vv := range *ll.ToArray() {
         if vv != expected[ii] {
-            t.Errorf("sorted linked list's node #%d should have value of %d", ii, expected[ii])
+            t.Errorf("Sorted linked list's node #%d should have value of %d", ii, expected[ii])
         }
     }
 }
@@ -45,7 +96,7 @@ func TestQuickSortEqualValues(t *testing.T) {
     ll.QuickSort()
     for ii, vv := range *ll.ToArray() {
         if vv != expected[ii] {
-            t.Errorf("sorted linked list's node #%d should have value of %d", ii, expected[ii])
+            t.Errorf("Sorted linked list's node #%d should have value of %d", ii, expected[ii])
         }
     }
 }
@@ -56,7 +107,7 @@ func TestQuickSortEqualValues2(t *testing.T) {
     ll.QuickSort()
     for ii, vv := range *ll.ToArray() {
         if vv != expected[ii] {
-            t.Errorf("sorted linked list's node #%d should have value of %d", ii, expected[ii])
+            t.Errorf("Sorted linked list's node #%d should have value of %d", ii, expected[ii])
         }
     }
 }
